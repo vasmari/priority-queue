2d3cdb2809df931aea199aeacd8bdad68003b455
Maryia Vasilkova <vasmari@users.noreply.github.com>
2019-10-16 02:50:26 +0300

    feat: add constructor and node methods

--- src/node.js
+++ src/node.js
@@ -1,23 +1,47 @@
 class Node {
 	constructor(data, priority) {
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 
 	}
 
 	appendChild(node) {
-
+		if (this.left === null){
+		  this.left  = node;
+		} else if (this.right === null){
+		  this.right = node;
+		} else {
+		  return;
+		}
+	
+		node.parent = this;
+	  }
+	
+	  removeChild(node) {
+		if (node === this.left){
+		  this.left = null;
+		} else if (node === this.right){
+		  this.right = null;
+		} else {
+		  throw new Error("passed node is not a child of this node");
+		}
+	
+		node.parent = null;
+	  }
+	
+	  remove() {
+		if (this.parent !== null){
+		  this.parent.removeChild(this);
+		}
+	  }
+	
+	  swapWithParent() {
+	  }
+	
+	
 	}
-
-	removeChild(node) {
-
-	}
-
-	remove() {
-
-	}
-
-	swapWithParent() {
-		
-	}
-}
-
-module.exports = Node;
+	
+	module.exports = Node;
